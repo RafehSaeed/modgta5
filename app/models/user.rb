b265b89b0e3cadb73e19b8d91a1e4dcaac1f93b5
RafehSaeed <rafeh_saeed@hotmail.ca>
2015-11-04 17:42:12 -0500

    Updates to controllers and models and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+has_many :items
+
+validates :name, presence : {message :"Name Required"}
+validates :password, presence : {message :"Password Needed"}
+validates :email, presence : {:"Email needed"}
+
+
+
 end
